d96dfdd445f0f5793ce97d88ac6913cabc79f62c
Vilhelm <Vilhelm@LENOVO-PC-VG>
2016-10-04 14:19:13 +0200

    BU!

--- ReceptView/ReceptView/Recipes.cs
+++ ReceptView/ReceptView/Recipes.cs
@@ -5,27 +5,19 @@ using System.Text;
 
 namespace ReceptView
 {
+    //public enum Category { Kött, Fisk, Sallader, Soppa, Dessert }
+    
     public class Recipes
     {
-        #region Properties
-
         public string Title { get; set; }
         public string Ingredients { get; set; }
         public string Author { get; set; }
         public string Instructions { get; set; }
         public string Category { get; set; }
-
-        #endregion
-
-        #region Constructor
-
-        ReadAndWrite rnw = new ReadAndWrite();
         List<Recipes> list = new List<Recipes>();
-        List<Recipes> results = new List<Recipes>();
+        ReadAndWrite rw = new ReadAndWrite();
 
-        #endregion
-
-        #region Methods
+        
 
         public bool ContainsInfo(string title, string author)
         {
@@ -42,24 +34,12 @@ namespace ReceptView
         public void Create(string title, string ingredients, string author, string instructions, string category)
         {
             list.Add(new Recipes { Title = title, Ingredients = ingredients, Author = author, Category = category, Instructions = instructions });
-            rnw.WriteToFile(title, ingredients, author, instructions, category);
         }
 
         public List<Recipes> Read(string searchText, string category)
         {
-            list = rnw.ReadToList();
-            if(category == "Alla")
-            {
-                results = list.Where(x => x.Title.Contains(searchText)).ToList();
-            }
-            else
-            {
-                results = list.Where(x => x.Title.Contains(searchText) && x.Category == category).ToList();
-            }
-            return results;
+            return list = rw.ReadToList();
         }
-
-        #endregion
 
         public void Update()
         {
