cef9c892feacddd50c1307c4968cd05f69e20498
Joakim Akerlund <joakim.akerlund@hotmail.com>
2016-10-04 11:25:24 +0200

    saverecipebtnclick

--- ReceptView/ReceptView/AddRecipe.cs
+++ ReceptView/ReceptView/AddRecipe.cs
@@ -16,5 +16,10 @@ namespace ReceptView
         {
             InitializeComponent();
         }
+
+        private void cmdSaveRecipe_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- ReceptView/ReceptView/Form1.cs
+++ ReceptView/ReceptView/Form1.cs
@@ -16,6 +16,7 @@ namespace ReceptView
         {
             InitializeComponent();
             
+            
         }
 
         private void cmdAddRecipe_Click(object sender, EventArgs e)
